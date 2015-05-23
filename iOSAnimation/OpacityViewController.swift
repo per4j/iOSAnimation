621df1ad1b1860a7655110aa6004091dea007cfd
per4j <panerchang@gmail.com>
2015-05-23 21:28:15 +0800

    add Opactity
     Changes to be committed:
    	modified:   Base.lproj/Main.storyboard
    	new file:   OpacityViewController.swift

---
+++ iOSAnimation/OpacityViewController.swift
@@ -0,0 +1,44 @@
+//
+//  OpacityViewController.swift
+//  iOSAnimation
+//
+//  Created by NSPer4j on 15/5/23.
+//  Copyright (c) 2015年 per4j. All rights reserved.
+//
+
+import UIKit
+
+class OpacityViewController: UIViewController {
+
+    @IBOutlet weak var greenSquare: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(1, animations: {
+            self.greenSquare.alpha = 0.4
+        })
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
